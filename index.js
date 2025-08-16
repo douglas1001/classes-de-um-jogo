6771c544d72309409603f582be3b492e3a0f8ffc
douglas1001 <douglassilva1001@gmail.com>
2025-08-16 15:10:27 -0300

    segundo commit

--- index.js
+++ index.js
@@ -6,10 +6,33 @@ class heroi {
     }
     
     atacar(){
-        console.log(`o ${this.tipo} atacou usando ${this.ataque}`)
+
+        let ataque;
+
+        switch(this.tipo){
+            case "mago":
+                ataque = "magia"
+                break;
+            case "guerreiro":
+                ataque = "espada"
+                break;
+            case "monge":
+                ataque = "artes marciais"
+                break;
+            case "ninja":
+                ataque = "shuriken"
+                break;
+        }
+        console.log(`o ${this.tipo} atacou usando ${ataque}`)
     }
 }
 
-let nomeHeroi = new heroi ("Douglas","30", "mago")
+let nomeHeroi = new heroi ("Gandalf","180", "mago")
+let nomeHeroi2 = new heroi ("Attila","35", "guerreiro")
+let nomeHeroi3 = new heroi ("Dalai Lama","89", "monge")
+let nomeHeroi4 = new heroi ("Douglas","30", "ninja")
 
 nomeHeroi.atacar()
+nomeHeroi2.atacar()
+nomeHeroi3.atacar()
+nomeHeroi4.atacar()
